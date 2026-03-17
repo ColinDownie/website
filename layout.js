95b0f2f9eee66eae798ab7cd9af1afffe621c6f3
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-03-17 14:42:56 +0000

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,3 +1,7 @@
+/* GLOBAL FONTS */
+document.write(`
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Playfair+Display:wght@400;600;700&display=swap" rel="stylesheet">
+`);
 /* PRELOADER */
 document.write(`
   <div id="preloader">
