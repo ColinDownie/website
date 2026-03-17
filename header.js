40a4221b8d10e32ec1bd3e7bac15df08f20d01fe
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-03-17 13:50:27 +0000

    Create header.js

---
+++ header.js
@@ -0,0 +1,37 @@
+document.write(`
+  <nav class="ross-nav">
+    <div class="ross-nav-logo">
+      <a href="index.html">ROSS HOUSE</a>
+    </div>
+
+    <ul class="ross-nav-links">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="site-map.html">Site Map</a></li>
+      <li><a href="contact.html">Contact</a></li>
+
+      <li class="dropdown">
+        <a href="#">Divisions</a>
+        <div class="dropdown-menu">
+          <a href="visual.html">Visual Concepts</a>
+          <a href="media.html">Media Concepts</a>
+          <a href="marketing.html">Marketing Concepts</a>
+          <a href="design.html">Design Concepts</a>
+          <a href="clothing.html">Clothing Concepts</a>
+          <a href="game.html">Game Concepts</a>
+          <a href="story.html">Story Concepts</a>
+          <a href="sound.html">Sound & Music</a>
+          <a href="experience.html">Experience Concepts</a>
+          <a href="future.html">Innovation & Futureform</a>
+          <a href="mentoring.html">Mentoring</a>
+          <a href="collab.html">Collaboration</a>
+          <a href="education.html">Education Concepts</a>
+          <a href="wellbeing.html">Wellbeing Concepts</a>
+          <a href="vector.html">VECTOR</a>
+          <a href="astra.html">ASTRA</a>
+          <a href="nova.html">NOVA</a>
+          <a href="guardian.html">Guardian Quadrant</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+`);
