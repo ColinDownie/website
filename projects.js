10619082f703659283b632cc46edc81c268658a3
Colin Downie <91563252+ColinDownie@users.noreply.github.com>
2026-03-18 07:18:36 +0000

    Update projects.js

--- projects.js
+++ projects.js
@@ -9,28 +9,3 @@ fadeEls.forEach(el => obs.observe(el));
 document.querySelector(".hor-sidebar-toggle").addEventListener("click", () => {
   document.querySelector(".hor-sidebar").classList.toggle("open");
 });
-
-// Filtering
-const filterBtns = document.querySelectorAll(".filter-btn");
-const cards = document.querySelectorAll(".project-card");
-
-filterBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-
-    filterBtns.forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
-
-    const filter = btn.dataset.filter;
-
-    cards.forEach(card => {
-      const categories = card.dataset.category.split(" ");
-
-      if (filter === "all" || categories.includes(filter)) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
-
-  });
-});
